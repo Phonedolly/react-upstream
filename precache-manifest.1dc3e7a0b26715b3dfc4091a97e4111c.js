5d57fb6395d230f3bd4b0b6a9177372796e69691
Phonedolly <Phonedolly@users.noreply.github.com>
2020-08-23 06:51:25 +0900

    Updates

---
+++ precache-manifest.1dc3e7a0b26715b3dfc4091a97e4111c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8912a102237ee8d1223149ceb1d8b3",
+    "url": "/react-upstream/index.html"
+  },
+  {
+    "revision": "f1f4ba87740ad68f0a72",
+    "url": "/react-upstream/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4cd952f40638436b064",
+    "url": "/react-upstream/static/js/2.686b8ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-upstream/static/js/2.686b8ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f4ba87740ad68f0a72",
+    "url": "/react-upstream/static/js/main.54b64227.chunk.js"
+  },
+  {
+    "revision": "07d12cf5d53b6c134316",
+    "url": "/react-upstream/static/js/runtime-main.d3f622e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-upstream/static/media/logo.5d5d9eef.svg"
+  }
+]);
